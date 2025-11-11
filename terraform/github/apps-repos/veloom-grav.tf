aefc0a6d704a66a3e728e96894899ae20e01ac30
alexvegas22 <alexvegas22@users.noreply.github.com>
2025-11-11 21:48:25 +0000

    [create-pull-request] automated change

---
+++ terraform/github/apps-repos/veloom-grav.tf
@@ -0,0 +1,49 @@
+resource "github_repository" "repo_veloom" {
+  name = "veloom.prodv2.cedille.club"
+  auto_init = true
+  homepage_url = "https://veloom.prodv2.cedille.club"
+  description = "Site web de veloom"
+  has_downloads = true
+  has_issues = true
+  has_projects = true
+  has_wiki = true
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+  topics = [ "grav" ]
+  vulnerability_alerts = true
+  visibility = "public"
+}
+
+resource "github_repository_webhook" "webhook_veloom" {
+  repository = github_repository.repo_veloom.name
+
+  configuration {
+    url          = "https://veloom.prodv2.cedille.club/_git_webhook"
+    content_type = "json"
+    insecure_ssl = false
+  }
+
+  active = true
+
+  events = ["push"]
+}
+
+resource "github_repository_dependabot_security_updates" "dependabot_veloom" {
+  repository  = github_repository.repo_veloom.name
+  enabled     = true
+}
+
+resource "github_repository_collaborators" "colaborators_veloom" {
+  repository = github_repository.repo_veloom.name
+
+  team {
+    permission = "admin"
+    team_id = "sre"
+  }
+}
