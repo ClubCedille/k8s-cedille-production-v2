16d613f8e68f871b110171c73e58fb410e2ba88a
CryoTechnic <cryogen65@gmail.com>
2026-06-12 16:12:24 -0400

    add default variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,10 +19,12 @@ variable "gh_pem" {
 variable "vault_address" {
   type        = string
   description = "URL of the Vault server (e.g. https://vault.prodv2.cedille.club)"
+  default     = "https://vault.prodv2.cedille.club"
 }
 
 variable "vault_token" {
   type        = string
   sensitive   = true
   description = "Vault token with write access to kv/ and sys/policy/ and auth/kubernetes/role/"
+  default     = ""
 }
